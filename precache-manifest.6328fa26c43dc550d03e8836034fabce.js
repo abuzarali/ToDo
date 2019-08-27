bf3269381ee8be63a0dd0492f50659d3b09ae77f
Abuzarali <am.peerbhai@gmail.com>
2019-08-27 20:03:17 +0530

    Updates

---
+++ precache-manifest.6328fa26c43dc550d03e8836034fabce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51f6b56a0814b8df322c",
+    "url": "/ToDo/static/js/runtime~main.51f6b56a.js"
+  },
+  {
+    "revision": "a10a875f5052a0b7b8dc",
+    "url": "/ToDo/static/js/main.a10a875f.chunk.js"
+  },
+  {
+    "revision": "a46c3e295f5ff88ce5c1",
+    "url": "/ToDo/static/js/1.a46c3e29.chunk.js"
+  },
+  {
+    "revision": "a10a875f5052a0b7b8dc",
+    "url": "/ToDo/static/css/main.dc57ef22.chunk.css"
+  },
+  {
+    "revision": "b9b387c19f7debc20d61275e989756a1",
+    "url": "/ToDo/index.html"
+  }
+];
